0de6c9da08bcc1e92d064399ce10a961cbc35889
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-13 16:54:03 +0200

    update

--- 4.2.tribonacci.js
+++ 4.2.tribonacci.js
@@ -1,10 +1,9 @@
 let tribonacci = (signature, n) => {
-  let arr = [];
-
-  for (i = 3; i <= n; i++) {
-    arr[i] = signature[i - 1] + signature[i - 2] + signature[i - 3];
+  let trib = signature;
+  for (i = 3; i < n; i++) {
+    trib.push((trib[i-1] + trib[i-2] + trib[i-3]));
   }
-  return arr;
+  return trib.slice(0, n);
 };
 
 console.log(tribonacci(9));
